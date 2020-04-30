d0443c57bdb0b19342dfed844018f4d18ba2c123
Michał Trętowicz <michal.tretowicz@droidsonroids.pl>
2020-04-30 23:38:27 +0200

    show splash only once per app launch

--- app/src/main/kotlin/pl/michal/tretowicz/ui/main/MainPresenter.kt
+++ app/src/main/kotlin/pl/michal/tretowicz/ui/main/MainPresenter.kt
@@ -35,25 +35,29 @@ class MainPresenter
 @Inject
 constructor(private val rxEventBus: RxEventBus, private val sessionManager: SessionManager, private val dataManager: DataManager) : BasePresenter<MainMvpView>() {
 
-
+    var shownSplashOnce = false
 
     override fun attachView(view: MainMvpView) {
         super.attachView(view)
 
-        view.showSplashScreen()
-        view.hideToolbar()
+        if(!shownSplashOnce) {
+            view.hideToolbar()
+            view.showSplashScreen()
+            Observable.just(true).delaySubscription(2, TimeUnit.SECONDS)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeBy(
+                            onNext = {
+                                shownSplashOnce = true
+                                view.showMainScreen()
+                                view.showToolbar()
+                            },
+                            onError = {
 
-        Observable.just(true).delaySubscription(2, TimeUnit.SECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy(
-                        onNext = {
-                            view.showMainScreen()
-                            view.showToolbar()
-                        },
-                        onError = {
-
-                        }
-                ).addTo(subscriptions)
+                            }
+                    ).addTo(subscriptions)
+        } else {
+            view.showMainScreen()
+        }
 
         rxEventBus.filteredObservable(EventShowMap::class.java)
                 .observeOn(AndroidSchedulers.mainThread())
